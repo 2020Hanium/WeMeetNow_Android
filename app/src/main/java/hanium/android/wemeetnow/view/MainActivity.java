6260d5daa23946a9bbd96fed78c738ccce5287df
leekangmin <kkk5474096@naver.com>
2020-08-18 14:44:49 +0900

    SplashActivity

--- app/src/main/java/hanium/android/wemeetnow/view/MainActivity.java
+++ app/src/main/java/hanium/android/wemeetnow/view/MainActivity.java
@@ -1,14 +1,16 @@
-package hanium.android.wemeetnow;
+package hanium.android.wemeetnow.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import hanium.android.wemeetnow.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
