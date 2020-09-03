9d85dad3165da527e6b9d706281e7b8a37a3d524
ChoiAhHyun <cah1113@naver.com>
2020-09-03 16:42:10 +0900

    MainActivity - 지도에 현재 위치 표시, 아이콘 추가

--- app/src/main/java/hanium/android/wemeetnow/view/MainActivity.java
+++ app/src/main/java/hanium/android/wemeetnow/view/MainActivity.java
@@ -2,31 +2,81 @@ package hanium.android.wemeetnow.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 import com.skt.Tmap.TMapView;
+
+import java.util.List;
 
 import hanium.android.wemeetnow.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Location currentLocation;
+
     private DrawerLayout drawerLayout;
     private EditText et_search;
+    private TMapView tmapview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getPermission();
         initialize();
         setMapView();
 
+    }
+
+    private void getPermission() {
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("[설정] > [권한] 에서 권한을 허용해주세요.")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+    }
+
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            getMyLocation();
+        }
+
+        @Override
+        public void onPermissionDenied(List<String> deniedPermissions) {
+
+        }
+    };
+
+    private void getMyLocation() {
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location != null) {
+                        currentLocation = location;
+                        tmapview.setCenterPoint(currentLocation.getLongitude(), currentLocation.getLatitude());
+                        tmapview.setIconVisibility(true);
+                        tmapview.setLocationPoint(currentLocation.getLongitude(), currentLocation.getLatitude());
+                    }
+                });
     }
 
     private void initialize() {
@@ -37,12 +87,15 @@ public class MainActivity extends AppCompatActivity {
 
         et_search = findViewById(R.id.et_search);
 
+        AppCompatImageButton btn_location = findViewById(R.id.btn_location);
+        btn_location.setOnClickListener(onClickListener);
+
         AppCompatImageButton btn_addparty = findViewById(R.id.btn_addparty);
         btn_addparty.setOnClickListener(onClickListener);
     }
 
     private void setMapView() {
-        TMapView tmapview = new TMapView(this);
+        tmapview = new TMapView(this);
         tmapview.setSKTMapApiKey(getString(R.string.tmap_app_key));
 
         RelativeLayout rl_tmap = findViewById(R.id.rl_tmap);
@@ -53,6 +106,10 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()){
             case R.id.btn_menu:{
                 drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            }
+            case R.id.btn_location:{
+                tmapview.setCenterPoint(currentLocation.getLongitude(), currentLocation.getLatitude(), true);
                 break;
             }
         }
