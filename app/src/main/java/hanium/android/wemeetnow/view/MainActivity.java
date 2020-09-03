5ae1bf2afd8b90f1999563a9b82f197288877894
ChoiAhHyun <cah1113@naver.com>
2020-09-03 15:23:00 +0900

    MainActivity - DrawerLayout 추가

--- app/src/main/java/hanium/android/wemeetnow/view/MainActivity.java
+++ app/src/main/java/hanium/android/wemeetnow/view/MainActivity.java
@@ -1,8 +1,13 @@
 package hanium.android.wemeetnow.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 import com.skt.Tmap.TMapView;
@@ -11,16 +16,54 @@ import hanium.android.wemeetnow.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private EditText et_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initialize();
+        setMapView();
+
+    }
+
+    private void initialize() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        AppCompatImageButton btn_menu = findViewById(R.id.btn_menu);
+        btn_menu.setOnClickListener(onClickListener);
+
+        et_search = findViewById(R.id.et_search);
+
+        AppCompatImageButton btn_addparty = findViewById(R.id.btn_addparty);
+        btn_addparty.setOnClickListener(onClickListener);
+    }
+
+    private void setMapView() {
         TMapView tmapview = new TMapView(this);
         tmapview.setSKTMapApiKey(getString(R.string.tmap_app_key));
 
         RelativeLayout rl_tmap = findViewById(R.id.rl_tmap);
         rl_tmap.addView(tmapview);
+    }
 
+    View.OnClickListener onClickListener = view -> {
+        switch (view.getId()){
+            case R.id.btn_menu:{
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            }
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
