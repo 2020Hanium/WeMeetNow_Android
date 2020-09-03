81997cc51f920fbef81fad63fc204eec02c46183
ChoiAhHyun <cah1113@naver.com>
2020-09-03 17:14:44 +0900

    자동 로그인 기능 추가

--- app/src/main/java/hanium/android/wemeetnow/view/SplashActivity.java
+++ app/src/main/java/hanium/android/wemeetnow/view/SplashActivity.java
@@ -6,14 +6,19 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashMap;
-
 import hanium.android.wemeetnow.R;
 import hanium.android.wemeetnow.etc.Constant;
+import hanium.android.wemeetnow.model.LoginModel;
+import hanium.android.wemeetnow.model.SuccessResponse;
+import hanium.android.wemeetnow.network.RetrofitInstance;
 import hanium.android.wemeetnow.util.PreferenceManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -27,11 +32,11 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Handler handler = new Handler();
-//        if (checkFirstLogin()) {
+        if (checkFirstLogin()) {
             handler.postDelayed(this::goToLogin, 1600);
-//        } else {
-//            requestLogin(id, pw);
-//        }
+        } else {
+            requestLogin();
+        }
     }
 
     private boolean checkFirstLogin() {
@@ -47,12 +52,41 @@ public class SplashActivity extends AppCompatActivity {
         finish();
     }
 
-    private void requestLogin(String id, String pw){
-        HashMap<String, String> map = new HashMap<>();
-        map.put("id", id);
-        map.put("passwd", pw);
+    private void requestLogin(){
+        RetrofitInstance.getInstance().getService().userLogin(new LoginModel(id, pw)).enqueue(new Callback<SuccessResponse>() {
+            @Override
+            public void onResponse(Call<SuccessResponse> call, Response<SuccessResponse> response) {
+                if (response.isSuccessful()) {
+                    SuccessResponse successResponse = response.body();
+                    if (successResponse != null) {
+                        int code = successResponse.code;
+                        String message = successResponse.message;
+                        if (code == 200) {
+                            saveUserInfo(id, pw);
+                            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                            Log.d("SplashActivity",  code + " " + message);
+                            goToLogin();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "잠시 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                        Log.d("SplashActivity",  response.code() + " " + response.message());
+                        goToLogin();
+                    }
+                }
+            }
 
-
+            @Override
+            public void onFailure(Call<SuccessResponse> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "잠시 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                Log.d("SplashActivity",  t.getMessage());
+                goToLogin();
+            }
+        });
     }
 
     private void saveUserInfo(String id, String pw){
