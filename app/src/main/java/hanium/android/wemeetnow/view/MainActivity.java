b1059801badfec4979ada49758df82db6fb9bdda
ChoiAhHyun <cah1113@naver.com>
2020-09-03 14:41:37 +0900

    Tmap API 추가 MainActivity - 검색창 CardView 수정, TmapView 추가

--- app/src/main/java/hanium/android/wemeetnow/view/MainActivity.java
+++ app/src/main/java/hanium/android/wemeetnow/view/MainActivity.java
@@ -3,6 +3,9 @@ package hanium.android.wemeetnow.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.skt.Tmap.TMapView;
 
 import hanium.android.wemeetnow.R;
 
@@ -12,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TMapView tmapview = new TMapView(this);
+        tmapview.setSKTMapApiKey(getString(R.string.tmap_app_key));
+
+        RelativeLayout rl_tmap = findViewById(R.id.rl_tmap);
+        rl_tmap.addView(tmapview);
+
     }
 }
